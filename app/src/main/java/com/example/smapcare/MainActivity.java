60f1f76661c37aac510e0ff7fbfd23210ef8812d
lakiah23mh <lakiah23.mh@gmail.com>
2019-10-29 16:58:28 +0700

    mengubah hampir semua

--- app/src/main/java/com/example/smapcare/MainActivity.java
+++ app/src/main/java/com/example/smapcare/MainActivity.java
@@ -3,12 +3,26 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+    EditText user, password;
+    Button login_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login_btn = findViewById(R.id.login_btn);
+
+//        login_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
 }
