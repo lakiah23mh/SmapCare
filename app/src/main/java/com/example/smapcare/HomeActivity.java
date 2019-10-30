8e9f135fbbee7cf37ca2ba8c68949c73af665555
lakiah23mh <lakiah23.mh@gmail.com>
2019-10-31 00:51:52 +0700

    Profile Data Ibu

--- app/src/main/java/com/example/smapcare/HomeActivity.java
+++ app/src/main/java/com/example/smapcare/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.smapcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        ImageView profilePhoto = findViewById(R.id.profile_image);
+//        profilePhoto.setImageResource(R.drawable.unnamed);
     }
 }
--- app/src/main/java/com/example/smapcare/MainActivity.java
+++ app/src/main/java/com/example/smapcare/MainActivity.java
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +19,14 @@ import android.widget.EditText;
         setContentView(R.layout.activity_main);
         login_btn = findViewById(R.id.login_btn);
 
-//        login_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
