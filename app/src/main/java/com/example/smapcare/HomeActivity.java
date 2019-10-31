8c6514ed27313724227748cee34f10e7d1efeb4a
lakiah23mh <lakiah23.mh@gmail.com>
2019-10-31 11:23:49 +0700

    Profile Data Ibu

--- app/src/main/java/com/example/smapcare/HomeActivity.java
+++ app/src/main/java/com/example/smapcare/HomeActivity.java
@@ -2,9 +2,12 @@ package com.example.smapcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,8 +15,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-//        ImageView profilePhoto = findViewById(R.id.profile_image);
-//        profilePhoto.setImageResource(R.drawable.unnamed);
     }
 }
