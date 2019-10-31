698be398c12de3b9f3ffdeb11a654daa6b265b1b
Samaya <val.samaya82@gmail.com>
2019-10-31 15:25:33 +0700

    layout list view + array judul konten

--- app/src/main/java/com/example/smapcare/KontenActivity.java
+++ app/src/main/java/com/example/smapcare/KontenActivity.java
@@ -3,8 +3,24 @@ package com.example.smapcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class KontenActivity extends AppCompatActivity {
+String [] judulKonten={
+        "Perawatan bayi baru lahir",
+        "Tanda-tanda bahaya bayi baru lahir",
+        "Masalah pada bayi",
+        "Masalah ibu nifas",
+        "ASI",
+        "Perubahan ibu nifas",
+        "Tanda-tanda ibu nifas",
+        "SC",
+        "Luka jalan lahir",
+        "Gizi ibu nifas",
+};
+
+ListView lv_konten;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
